a47079cb5f44a20aa0c29dacf2fb51adf6ab94bf
Daniel D'Avella <drdavella@gmail.com>
2018-09-27 15:15:48 -0400

    Add packages=find_packages() to setup script

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='tox-conda',
     version='0.1.0',
-    py_modules=['tox_conda'],
     entry_points=dict(tox=['conda = tox_conda.plugin']),
     classifiers=['Framework:: tox'],
+    packages=find_packages(),
 )
