6fe8df20a55562301208445015f9d5fd9369f5af
Daniel D'Avella <drdavella@gmail.com>
2018-09-27 10:26:46 -0400

    Update package layout

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     name='tox-conda',
     version='0.1.0',
     py_modules=['tox_conda'],
-    entry_points=dict(tox=['conda = tox_conda']),
+    entry_points=dict(tox=['conda = tox_conda.plugin']),
     classifiers=['Framework:: tox'],
 )
