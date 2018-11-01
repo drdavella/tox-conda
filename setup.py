c60b67efb1de3db32730493ea86144a5401b37f5
Daniel D'Avella <ddavella@stsci.edu>
2018-11-01 16:09:34 -0400

    Update setup.py in preparation for release

--- setup.py
+++ setup.py
@@ -2,10 +2,20 @@
 from setuptools import setup, find_packages
 
 
+with open('README.rst') as ff:
+    readme = ff.read()
+
 setup(
     name='tox-conda',
     version='0.1.0',
+    description='Tox plugin that provides integration with conda',
+    long_description=readme,
+    author="Daniel R. D'Avella",
+    author_email='ddavella@stsci.edu',
+    url='https://github.com/drdavella/tox-conda',
     entry_points=dict(tox=['conda = tox_conda.plugin']),
     classifiers=['Framework:: tox'],
+    install_requires=['tox'],
+    python_requires='>=3.3',
     packages=find_packages(),
 )
